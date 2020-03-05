75c0d96abfa8861b5795988d1f854c5e6f0208b0
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2020-03-05 23:30:27 +0900

    Initialize ch-3-ex-2

--- ch-3-ex-2/client.rb
+++ ch-3-ex-2/client.rb
@@ -14,22 +14,29 @@ CLIENT_ID = 'oauth-client-1'
 CLIENT_SECRET = 'oauth-client-secret-1'
 
 REDIRECT_URIS = %w[http://localhost:9000/callback]
+SCOPE = 'foo'
 
 PROTECTED_RESOURCE = 'http://localhost:9002/resource'
 
 state = nil
-access_token = nil
+
+access_token = '987tghjkiu6trfghjuytrghj'
+scope = nil
+refresh_token = 'j2r3oj32r23rmasd98uhjrk2o3i'
 
 get '/' do
-  erb :index, locals: { access_token: access_token }
+  erb :index, locals: { access_token: access_token, scope: scope, refresh_token: refresh_token }
 end
 
 get '/authorize' do
   access_token = nil
+
+  scope = nil
   state = SecureRandom.urlsafe_base64
 
   query = build_query(
     response_type: 'code',
+    scope: SCOPE,
     client_id: CLIENT_ID,
     redirect_uri: REDIRECT_URIS.first,
     state: state,
@@ -52,19 +59,21 @@ get '/callback' do
     redirect_uri: REDIRECT_URIS.first,
   )
 
-  if response.code == '200'
+  case response
+  when Net::HTTPSuccess
     body = JSON.parse(response.body)
-    access_token = body['access_token']
 
-    erb :index, locals: { access_token: access_token }
+    access_token = body['access_token']
+    refresh_token = body['refresh_token'] if body['refresh_token']
+    scope = body['scope']
+
+    erb :index, locals: { access_token: access_token, scope: scope, refresh_token: refresh_token }
   else
     "Unable to fetch access token, server response: #{response.code}"
   end
 end
 
 get '/fetch_resource' do
-  halt 'Missing access token' if access_token.nil?
-
   protected_resource_uri = URI.parse(PROTECTED_RESOURCE)
   http = Net::HTTP.new(protected_resource_uri.host, protected_resource_uri.port)
   headers = { 'Authorization' => "Bearer #{access_token}" }
@@ -72,9 +81,11 @@ get '/fetch_resource' do
   logger.info "Requesting protected resource with access token '#{access_token}'"
   response = http.post(protected_resource_uri.path, nil, headers)
 
-  if response.code == '200'
+  case response
+  when Net::HTTPSuccess
     response.body
   else
+    # TODO
     access_token = nil
     "Unable to fetch resource, server response: #{response.code}"
   end

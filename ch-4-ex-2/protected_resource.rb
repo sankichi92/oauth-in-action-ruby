d018a5c3391574274e9c56e504f3fae5dfdc0ab3
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2020-03-07 00:09:56 +0900

    cp -R ch-4-ex-1 ch-4-ex-2

---
+++ ch-4-ex-2/protected_resource.rb
@@ -0,0 +1,23 @@
+require 'json'
+
+require 'sinatra'
+require 'sinatra/json'
+
+DATA_PATH = File.expand_path('../oauth-in-action-code/exercises/ch-4-ex-1/database.nosql', __dir__)
+
+RESOURCE = {
+  name: 'Protected Resource',
+  description: 'This data has been protected by OAuth 2.0'
+}
+
+set :port, 9002
+
+before do
+  access_token = request.env['HTTP_AUTHORIZATION']&.slice(%r{^Bearer +([a-z0-9\-._‾+/]+=*)}i, 1) || params[:access_token]
+  logger.info "Incoming token: #{access_token}"
+  error 401 if access_token.nil? || File.open(DATA_PATH).none? { |line| access_token == JSON.parse(line).fetch('access_token') }
+end
+
+post '/resource' do
+  json RESOURCE
+end

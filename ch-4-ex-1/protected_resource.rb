5b95b9f561cef4b14523a1aa5e5ca2076ac1ce2f
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2020-03-07 00:04:47 +0900

    Use 'sinatra/json' in ch-4-ex-1

--- ch-4-ex-1/protected_resource.rb
+++ ch-4-ex-1/protected_resource.rb
@@ -1,8 +1,14 @@
 require 'json'
 
 require 'sinatra'
+require 'sinatra/json'
 
 DATA_PATH = File.expand_path('../oauth-in-action-code/exercises/ch-4-ex-1/database.nosql', __dir__)
+
+RESOURCE = {
+  name: 'Protected Resource',
+  description: 'This data has been protected by OAuth 2.0'
+}
 
 set :port, 9002
 
@@ -13,8 +19,5 @@ before do
 end
 
 post '/resource' do
-  {
-    name: 'Protected Resource',
-    description: 'This data has been protected by OAuth 2.0'
-  }.to_json
+  json RESOURCE
 end

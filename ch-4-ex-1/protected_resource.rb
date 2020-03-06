56096fb12dc850b9f16939cebc058a8117e2dee4
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2020-03-06 23:56:02 +0900

    Solve ch-4-ex-1

--- ch-4-ex-1/protected_resource.rb
+++ ch-4-ex-1/protected_resource.rb
@@ -7,7 +7,9 @@ DATA_PATH = File.expand_path('../oauth-in-action-code/exercises/ch-4-ex-1/databa
 set :port, 9002
 
 before do
-  # TODO
+  access_token = request.env['HTTP_AUTHORIZATION']&.slice(%r{^Bearer +([a-z0-9\-._‾+/]+=*)}i, 1) || params[:access_token]
+  logger.info "Incoming token: #{access_token}"
+  error 401 if access_token.nil? || File.open(DATA_PATH).none? { |line| access_token == JSON.parse(line)['access_token'] }
 end
 
 post '/resource' do

5c69cb9dd8cfb945c1c8ac59fa7c6c5df97bad1a
Takahiro Miyoshi <takahiro-miyoshi@sankichi.net>
2020-03-08 15:58:29 +0900

    Initialize ch-5-ex-1

---
+++ ch-5-ex-1/authorization_server.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require 'sinatra'
+
+CLIENTS = [
+  # TODO
+].freeze
+
+set :port, 9001
+
+template :approve do
+  <<~HTML
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <title>Authorization Server</title>
+      </head>
+      <body>
+        <p>Approve this client?</p>
+        <ul>
+          <li>ID: <%= @client.id %></li>
+        </ul>
+        <form action="/approve" method="POST">
+          <input type="hidden" name="request_id" value="<%= @request_id %>">
+          <input type="submit" name="approve" value="Approve">
+          <input type="submit" name="deny" value="Deny">
+        </form>
+      </body>
+    </html>
+  HTML
+end
+
+get '/authorize' do
+  # TODO
+end
+
+post '/approve' do
+  # TODO
+end
+
+post '/token' do
+  # TODO
+end
